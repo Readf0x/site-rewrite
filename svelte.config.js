23c9efc3ed15c69dfd0c8942a904a0c8c733faa7
readf0x <davis.a.forsythe@gmail.com>
2023-11-02 20:34:08 -0500

    fixed svelte conf

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
-import preprocess from "svelte-preprocess";
+import sveltePreprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from "@sveltejs/adapter-auto";
-import type { Config } from "@sveltejs/kit";
 
-const config: Config = {
-  preprocess: preprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [ sveltePreprocess(), vitePreprocess() ],
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
