a1e0c4bf96cad372d41548079ba73ce3a62de519
Readf0x <davis.a.forsythe@gmail.com>
2023-11-16 14:49:08 -0600

    things are not going well

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -1,13 +1,38 @@
 import { json } from "@sveltejs/kit"
+import { remark } from "remark"
+import strip from "strip-markdown"
+import gfm from "remark-gfm"
 
-type SearchResults = {
-  query: string
-  results: Array<{
-    page: string
-    preview: string
-  }>
+export type SearchResultItem = {
+  route: string
+  context: string
 }
 
-export const GET = ({ url }) => {
-  return json({q: url.searchParams.get("q")})
+export type SearchResults = {
+  query: string
+  results: SearchResultItem[]
+}
+
+const pages = import.meta.glob("$lib/pages/*.md", { as: "raw", eager: true })
+
+const search = async function(query: string): Promise<SearchResultItem[]> {
+  let results: SearchResultItem[] = new Array
+  for (const page in pages) {
+    const route: string = page.match(/\w+(?=\.md)/)?.[0] || ""
+
+    const plain = await remark()
+      .use(gfm)
+      .use(strip)
+      .process(pages[page])
+
+    const result = { route, context: "Not Implemented" } satisfies SearchResultItem
+
+    results.push(result)
+  }
+  return results
+}
+
+export const GET = async ({ url }) => {
+  const query = url.searchParams.get("q") || "No Search"
+  return json({ query, results: await search(query) } satisfies SearchResults)
 }
